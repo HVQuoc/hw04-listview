71edf2d563c127cb2296c8363c1bcb94d73599ab
21120581 <thuytruc011258@gmail.com>
2023-10-13 01:44:08 +0700

    create and custom ListView

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.listview;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
-import java.util.ArrayList;
+import android.widget.Button;
+import android.widget.AdapterView;
 
 // a static class as a random info machine
 class RandomDataMachine {
@@ -19,7 +21,7 @@ class RandomDataMachine {
     String [] lastNameRepo = {"Tran", "Lam", "Nguyen", "Le", "Huynh"};
     String [] middleNameRepo = {"Thao", "Minh", "Thuy", "Phuong", "Thu"};
     Integer [] thumnailsRepo = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5,
-                            R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10};
+            R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10};
 
     // instances
     Integer image;
@@ -54,30 +56,46 @@ class RandomDataMachine {
     }
 }
 
-public class MainActivity extends ListActivity {
-        TextView txtMsg;
-        RandomDataMachine item = new RandomDataMachine();
-// The n-th row in the list will consist of [icon, label] where icon = thumbnail[n] and label=items[n]
-        String[] names = { item.randomName(), "Nguyen Van B", "Nguyen Van C", "Nguyen Van D" , "Nguyen Van E" };
-        String [] phones = {item.randomPhoneNumber(),"0867137365","0867137365","0867137365","0867137365"};
-        Integer[] thumbnails = {item.randomThumnails(), R.drawable.pic2, R.drawable.pic3,R.drawable.pic4,R.drawable.pic5 };
+public class MainActivity extends Activity {
+    Button btnGen;
+    EditText edtLine;
+    Spinner spnPage;
+    TextView txtChosen;
+    ListView list;
+    RandomDataMachine item = new RandomDataMachine();
+    // The n-th row in the list will consist of [icon, label] where icon = thumbnail[n] and label=items[n]
+    String[] names = { item.randomName(), "Nguyen Van B", "Nguyen Van C", "Nguyen Van D" , "Nguyen Van E" };
+    String [] phones = {item.randomPhoneNumber(),"0867137365","0867137365","0867137365","0867137365"};
+    Integer[] thumbnails = {item.randomThumnails(), R.drawable.pic2, R.drawable.pic3,R.drawable.pic4,R.drawable.pic5 };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            txtMsg = (TextView) findViewById(R.id.txtMsg);
-// the arguments of the custom adapter are: activityContex, layout-to-be-inflated, labels, icons
-            CustomIconLabelAdapter adapter = new CustomIconLabelAdapter(this, R.layout.custom_list_view, names, phones, thumbnails);
-// bind intrinsic ListView to custom adapter
-            setListAdapter(adapter);
-        }//onCreate
-        @Override
-        protected void onListItemClick(ListView l, View v, int position, long id) {
-            super.onListItemClick(l, v, position, id);
-            txtMsg.setText(" Position: " + position + " " + names[position]);
-    }
+        btnGen = (Button) findViewById(R.id.btnGen);
+        edtLine = (EditText) findViewById(R.id.edtLine);
+
+        spnPage = (Spinner) findViewById(R.id.spnPage);
+        String[] pageList={"1","2","3"}; //just for testing
+        ArrayAdapter<String> spinAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, pageList);
+        spnPage.setAdapter(spinAdapter);
+
+        txtChosen = (TextView) findViewById(R.id.txtChosen);
+        list = (ListView) findViewById(R.id.list);
+
+        // the arguments of the custom adapter are: activityContex, layout-to-be-inflated, labels, icons
+        CustomIconLabelAdapter adapter = new CustomIconLabelAdapter(this, R.layout.custom_list_view, names, phones, thumbnails);
+        // bind intrinsic ListView to custom adapter
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int position, long id) {
+                txtChosen.setText(names[position]);
+            }
+        });
+    }//onCreate
 }
 
 class CustomIconLabelAdapter extends ArrayAdapter<String> {
@@ -93,11 +111,11 @@ class CustomIconLabelAdapter extends ArrayAdapter<String> {
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = ((Activity) context).getLayoutInflater();
         View row = inflater.inflate(R.layout.custom_list_view, null);
-        TextView label = (TextView) row.findViewById(R.id.label);
-        TextView label2 = (TextView) row.findViewById(R.id.label2);
+        TextView name = (TextView) row.findViewById(R.id.name);
+        TextView phone = (TextView) row.findViewById(R.id.phone);
         ImageView icon = (ImageView) row.findViewById(R.id.icon);
-        label.setText(items[position]);
-        label2.setText(item2s[position]);
+        name.setText(items[position]);
+        phone.setText(item2s[position]);
         icon.setImageResource(thumbnails[position]);
         return (row);
     }
