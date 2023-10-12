06a232f847ae03a1d4893824ed82a15de554520d
Quoc Hoang <hoangvanquoc923@gmail.com>
2023-10-12 08:51:51 +0700

    add class RandomDataMachine

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -1,6 +1,4 @@
-
 package com.example.listview;
-
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
@@ -12,20 +10,66 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import java.util.ArrayList;
+
+// a static class as a random info machine
+class RandomDataMachine {
+    // random repos
+    String [] firstNameRepo = {"Quoc", "Truc", "Hoang", "Quan", "Huyen", "Hoa", "My", "Dung", "Mai", "Lan"};
+    String [] lastNameRepo = {"Tran", "Lam", "Nguyen", "Le", "Huynh"};
+    String [] middleNameRepo = {"Thao", "Minh", "Thuy", "Phuong", "Thu"};
+    Integer [] thumnailsRepo = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5,
+                            R.drawable.pic6, R.drawable.pic7, R.drawable.pic8, R.drawable.pic9, R.drawable.pic10};
+
+    // instances
+    Integer image;
+    private String name;
+    private String phoneNumber;
+    public String getName() {
+        return name;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public Integer getImage() {
+        return image;
+    }
+
+    public String randomName(){
+        int index1 = (int)Math.floor(Math.random() * this.lastNameRepo.length);
+        int index2 = (int)Math.floor(Math.random() * this.middleNameRepo.length);
+        int index3 = (int)Math.floor(Math.random() * this.firstNameRepo.length);
+        return lastNameRepo[index1] + " " + middleNameRepo[index2] + " " + firstNameRepo[index3];
+    }
+    public String randomPhoneNumber() {
+        String phoneNumber = "0";
+        for (int i = 0; i < 9; ++i) {
+            phoneNumber += (int)Math.floor(Math.random() * 9);
+        }
+        return phoneNumber;
+    }
+    public Integer randomThumnails() {
+        int index = (int)Math.floor(Math.random() * this.thumnailsRepo.length);
+        return thumnailsRepo[index];
+    }
+}
 
 public class MainActivity extends ListActivity {
         TextView txtMsg;
+        RandomDataMachine item = new RandomDataMachine();
 // The n-th row in the list will consist of [icon, label] where icon = thumbnail[n] and label=items[n]
-        String[] names = { "Nguyen Van A", "Nguyen Van B", "Nguyen Van C", "Nguyen Van D" , "Nguyen Van E" };
-        String [] phones = {"0867137365","0867137365","0867137365","0867137365","0867137365"};
-        Integer[] thumbnails = { R.drawable.pic01_small, R.drawable.pic02_small, R.drawable.pic03_small,R.drawable.pic04_small,R.drawable.pic05_small };
+        String[] names = { item.randomName(), "Nguyen Van B", "Nguyen Van C", "Nguyen Van D" , "Nguyen Van E" };
+        String [] phones = {item.randomPhoneNumber(),"0867137365","0867137365","0867137365","0867137365"};
+        Integer[] thumbnails = {item.randomThumnails(), R.drawable.pic2, R.drawable.pic3,R.drawable.pic4,R.drawable.pic5 };
+
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             txtMsg = (TextView) findViewById(R.id.txtMsg);
 // the arguments of the custom adapter are: activityContex, layout-to-be-inflated, labels, icons
-            CustomIconLabelAdapter adapter = new CustomIconLabelAdapter(this, R.layout.custom_list_view, names,phones, thumbnails);
+            CustomIconLabelAdapter adapter = new CustomIconLabelAdapter(this, R.layout.custom_list_view, names, phones, thumbnails);
 // bind intrinsic ListView to custom adapter
             setListAdapter(adapter);
         }//onCreate
